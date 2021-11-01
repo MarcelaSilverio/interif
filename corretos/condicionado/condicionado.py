3fa77ab1e91d556047032fe72fe242377b2c1324
Bruna Sanches <50997147+BrunaSanches@users.noreply.github.com>
2021-11-01 16:47:19 -0300

    Add files via upload

---
+++ corretos/condicionado/condicionado.py
@@ -0,0 +1,20 @@
+# ** Alunas ****************************
+# Bruna C. Sanches ------(201811130054)
+# Izabela A. Andrade ---- (20192004795)
+# Marcela P. Silvério ----(20192020028)
+# Tássyla L. Lima ------- (20192001990)
+# Turma: INF3A
+# **************************************
+temp=str(input().upper())
+if(temp=="FRIO"):
+    print("24")
+elif(temp=="MUITO FRIO"):
+    print("20")
+elif(temp=="EXTREMAMENTE FRIO"):
+    print("18")
+elif(temp=="QUENTE"):
+    print("28")
+else:
+    print("COMANDO INVALIDO")
+
+
