8e5eaac24d0d929dc0c75ba7478bb1e0f187b6b0
Izabela A. Andrade (bela) <contatobelaaa@gmail.com>
2021-11-01 15:36:22 -0300

    Problema D - Fritadeira da Vovó

---
+++ corretos/D_Fritadeira/fritadeira.py
@@ -0,0 +1,14 @@
+# **** Alunas *************************
+# Bruna C. Sanches ----- (201811130054)
+# Izabela A. Andrade ---- (20192004795)
+# Marcela P. Silvério --- (20192020028)
+# Tássyla L. Lima ------- (20192001990)
+# Turma: INF3A
+# *************************************
+import math
+
+temperatura_c = int(input())
+
+temperatura_f = math.floor(temperatura_c * (9/5) + 32)
+
+print("{0} graus celsius equivale a {1} graus fahrenheit".format(temperatura_c, temperatura_f))
