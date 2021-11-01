73ae18792bcf6eacaa4c6d72e420d82cb44eaf32
Marcela Prata Silvério <marcelapsilverio@gmail.com>
2021-11-01 17:07:01 -0300

    Figurinhas Decimal

---
+++ errados/condicionado.py
@@ -0,0 +1,20 @@
+# **** Alunas *************************
+# Bruna C. Sanches ------(201811130054)
+# Izabela A. Andrade ---- (20192004795)
+# Marcela P. Silvério ----(20192020028)
+# Tássyla L. Lima ------- (20192001990)
+# Turma: INF3A
+# *************************************
+
+nebulosidade = input()
+
+if nebulosidade == "FRIO":
+    print(24)
+elif nebulosidade == "MUITO FRIO":
+    print(20)
+elif nebulosidade == "EXTREMAMENTE FRIO":
+    print(18)
+elif nebulosidade == "QUENTE":
+    print(28)
+else:
+    print("COMANDO INVALIDO")
