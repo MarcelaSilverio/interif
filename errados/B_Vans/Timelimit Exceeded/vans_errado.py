7df88c80bcf106a93178b869f980fcd7ab6304a4
Izabela A. Andrade (bela) <contatobelaaa@gmail.com>
2021-11-01 16:36:30 -0300

    Create vans_errado.py

---
+++ errados/B_Vans/Timelimit Exceeded/vans_errado.py
@@ -0,0 +1,16 @@
+# **** Alunas *************************
+# Bruna C. Sanches ------(201811130054)
+# Izabela A. Andrade ---- (20192004795)
+# Marcela P. Silvério ----(20192020028)
+# Tássyla L. Lima ------- (20192001990)
+# Turma: INF3A
+# *************************************
+
+import math
+import time
+
+tamanho_percurso, velocidade_rogerinho, tempo_corrida = map(int, input().split())
+tamanho_percurso_km = tamanho_percurso/1000
+saida = (velocidade_rogerinho/tamanho_percurso_km)*tempo_corrida
+time.sleep(60) #Erro de Tempo Limite Excedido Induzido
+print(math.floor(saida))
